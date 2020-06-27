789543ca45d68cde0e46ea5b203ee4e5c0874fe3
abi11 <abhiyendra5@yahoo.com>
2020-06-27 02:24:14 -0400

    existing project

---
+++ src/main/java/com/abhi/spring/batch/CustomItemProcessor.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.abhi.spring.batch;
+
+
+import org.springframework.batch.item.ItemProcessor;
+
+public class CustomItemProcessor implements ItemProcessor<Report, Report> {
+	public Report process(Report itemObj) throws Exception {
+		System.out.println("Processing Item?= " + itemObj);
+		return itemObj;
+	}
+}
